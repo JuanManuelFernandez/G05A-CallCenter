9baae26c90c72e8bbf7767af607f5ccd4849900a
facuello <facuello5@gmail.com>
2025-06-03 19:33:19 -0300

    fix

--- TPIDataBaste.sql
+++ TPIDataBaste.sql
@@ -3,4 +3,12 @@ GO
 USE TPI_CallCenter
 GO
 
-CREATE TABLE 
+CREATE TABLE Incidencias(
+	IdIncidencia INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	LegajoEmpleado VARCHAR(7) NOT NULL,
+	DNICliente Varchar(20) NOT NULL,
+	Descripcion VARCHAR(500) NOT NULL,
+	FechaYHora datetime NOT NULL,
+	IdPrioridad INT NOT NULL,
+	Estado BIT NOT NULL
+)
