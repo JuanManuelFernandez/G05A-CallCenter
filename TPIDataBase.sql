752ead78ab0fa79a78dd8c8245f40ebc8a044f74
Steve <theRealSteveGates@outlook.com>
2025-06-07 12:21:48 -0300

    Modificaciones a la base de datos

--- TPIDataBase.sql
+++ TPIDataBase.sql
@@ -1,97 +1,73 @@
---CREATE DATABASE TPI_CallCenter
---GO
---USE TPI_CallCenter
---GO
+CREATE DATABASE TPI_CallCenter
+GO
+USE TPI_CallCenter
+GO
 
---CREATE TABLE Incidencias(
---	IdIncidencia INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
---	LegajoEmpleado VARCHAR(7) NOT NULL,
---	DNICliente Varchar(20) NOT NULL, -- Reemplazar por IDCliente?
---	IdTipo INT NOT NULL, -- Hacer tabla aparte de Tipos de Incidencia con un ID
---	--EstadoActual VARCHAR()
---	Descripcion VARCHAR(500) NOT NULL,
---	FechaYHoraCreacion datetime NOT NULL,
---	FechaYHoraResolucion datetime NULL,
---	Resolucion VARCHAR(500) NULL,
---	IdPrioridad INT NOT NULL, -- Debatir convecion
---	Estado VARCHAR(50) NOT NULL
---)
+DROP TABLE Incidencias
 
---PRUEBA PARA TABLA CLIENTES
---CREATE TABLE Cliente (
---	IDPersona INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
---	DNI NVARCHAR(50) NOT NULL,
---	Nombre NVARCHAR(50) NOT NULL,
---	Apellido NVARCHAR(50) NOT NULL,
---	Mail NVARCHAR(100) NOT NULL,
---	Telefono NVARCHAR(50) NOT NULL,
---	Contraseña NVARCHAR(50) NOT NULL
---)
+CREATE TABLE Incidencias(
+	IDIncidencia INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	IDEmpleado INT NOT NULL, --FOREIGN KEY REFERENCES Empleados(IDEmpleado),
+	IDCliente INT NOT NULL, --FOREIGN KEY REFERENCES Clientes(IDCliente),
+	IDTipo INT NOT NULL, -- FOREIGN KEY REFERENCES TiposIncidente(IDTipo),
+	IDPrioridad INT NOT NULL, -- FOREIGN KEY REFERENCES PrioridadesIncidente(IDPrioridad),
+	EstadoActual VARCHAR(250) NOT NULL,
+	Descripcion NVARCHAR(500) NOT NULL,
+	FechaYHoraCreacion datetime NOT NULL,
+	FechaYHoraResolucion datetime NULL,
+	Resolucion NVARCHAR(500) NULL
+)
 
-SELECT * FROM Cliente;
+INSERT INTO Incidencias (IDEmpleado, IDCliente, IDTipo, IDPrioridad, EstadoActual, Descripcion, FechaYHoraCreacion, FechaYHoraResolucion, Resolucion)
+VALUES (7, 3, 1, 1, 'Enviamos personal para reemplazar el modem', 'El cliente no uso el adaptador correcto y quemo la fuente del modem', GETDATE(), NULL, NULL),
+	   (7, 5, 3, 2, 'Iniciamos proceso de reinicio de fabrica remoto', 'Firmware del modem corrompido tras una actualizacion', GETDATE(), NULL, NULL)
 
---DELETE FROM Cliente Where IDPersona = 9;
+SELECT * FROM Incidencias
 
---INSERT INTO Cliente (DNI, Nombre, Apellido, Mail, Telefono, Contraseña)
---VALUES ('12345678A', 'Juan', 'Pérez', 'juan.perez@example.com', '123456789', 'miContraseñaSegura');
-
---DROP TABLE Cliente;
-
---SELECT * FROM Incidencias
-
---INSERT INTO Incidencias (LegajoEmpleado, DNICliente, IdTipo, Descripcion, FechaYHoraCreacion, FechaYHoraResolucion, Resolucion, IdPrioridad, Estado)
---	VALUES ('e98114', '46286378', 1, 'Rotura de Modem', GETDATE(), NULL, NULL, 1, 'En Progreso')
-
---table persona{
---  IDPersona int
---  Nombre string
---  Apellido string
---  Contraseña string
---  Mail string
---  Telefono int
+--CREATE TABLE TiposIncidente{
+--  IDTipo INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+--  Descripcion string
 --}
 
---table clientes{
---  Dni int
---  Categoria categoria -- Regular, Plus, Premium
+--CREATE TABLE PrioridadesIncidente{
+--  IDPrioridad INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+--  Descripcion VARCHAR
 --}
 
---table empleados{
+CREATE TABLE Clientes (
+	IDCliente INT PRIMARY KEY IDENTITY(1,1) NOT NULL, -- CAMBIO
+	DNI INT NOT NULL,
+	Nombre NVARCHAR(50) NOT NULL,
+	Apellido NVARCHAR(50) NOT NULL,
+	Mail NVARCHAR(100) NOT NULL,
+	Telefono NVARCHAR(50) NOT NULL,
+	Contraseña NVARCHAR(50) NOT NULL,
+	IDCategoria INT NOT NULL -- CAMBIO
+)
+
+INSERT INTO Clientes (DNI, Nombre, Apellido, Mail, Telefono, Contraseña, IDCategoria)
+VALUES ('45444555', 'Juan', 'Pérez', 'juan.perez@example.com', '11-3255-9638', 'miContraseñaSegura', 2),
+	   ('44333222', 'Jose', 'Diaz', 'jose.diaz@example.com', '11-3324-3234', 'aguanteMessi', 3)
+
+--CREATE TABLE Categorias {
+--  IDCategoria INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+--  Nombre NVARCHAR(50) NOT NULL, -- Regular, Plus, Premium, etc. Determina el tiempo de respuesta minimo por incidencia
+--}
+
+--CREATE TABLE Empleados {
+--	IDEmpleado INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
 --  Legajo string
---  Perfil string
+--	IDCliente INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+--  Dni INT NOT NULL,
+--	Nombre VARCHAR(50) NOT NULL,
+--	Apellido VARCHAR(50) NOT NULL,
+--	Mail VARCHAR(100) NOT NULL,
+--  IdCategoria INT NOT NULL -- Regular, Plus, Premium
 --}
 
---table incidencias{
---  IDIncidencias int
---  LegajoEmpleado string
---  DNICliente string
---  Tipo Tipo
---  Descripcion string
---  FechaYHoraCreacion datetime
---  FechaYHoraResolucion datetime
---  Resolucion string
---  Estado string
---  Prioridad prioridad
---}
-
---Table tipoIncidente{
---  IdTipo int
---  Descripcion string
---}
-
---table historial{
+--CREATE TABLE historial{
 --  IDHistorial int
 --  IDIncidencias int
 --  Legajo string
 --  FechaCambio datetime
---}
-
---table prioridad{
---  IDPrioridad int
---  Descripcion string
---}
-
---table categoria {
---  IDCategoria int
---  Descripcion string
 --}
