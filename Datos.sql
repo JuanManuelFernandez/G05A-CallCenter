aafde1b51f58350e2b710a3322b83c0c7e0fabbc
Julian Pierri <estudios.w6qo3@silomails.com>
2025-06-26 05:27:12 -0700

    Actualizados datos de prueba para simplificar pruebas

--- Datos.sql
+++ Datos.sql
@@ -1,35 +1,59 @@
 GO
 USE CallCenter
 GO
+
+DELETE FROM Historiales
+DELETE FROM Incidencias
+DELETE FROM PrioridadesIncidente
+DELETE FROM TiposIncidente
+DELETE FROM Empleados
+DELETE FROM Clientes
+DELETE FROM CategoriasCliente
+DELETE FROM Usuarios
+
+--SELECT * FROM CategoriasCliente
+--DELETE Usuarios
+
 INSERT INTO Usuarios (TipoUsuario, Email, Clave) VALUES
-(3, 'juan.perez@mail.com', 'clave123'),
-(3, 'ana.garcia@mail.com', 'secreta456'),
-(2, 'julian.alvarez@mail.com', 'empleado'),
-(1, 'leo.messi@mail.com', 'admin')
+(3, 'usuario1@mail.com', 'clave1'),
+(3, 'usuario2@mail.com', 'clave2'),
+(3, 'usuario3@mail.com', 'clave3'),
+(2, 'empleado1@mail.com', 'clave1'),
+(2, 'empleado2@mail.com', 'clave2'),
+(2, 'empleado3@mail.com', 'clave3'),
+(1, 'admin@mail.com', 'clave1')
 
 INSERT INTO CategoriasCliente (Nombre, Descripcion) VALUES
 ('Regular', 'Atención estándar en menos de 72h'),
 ('Super', 'Atención mejorada en menos de 48h'),
-('Premium', 'Atención prioritaria en menos de 24h');
+('Premium', 'Atención prioritaria en menos de 24h')
 
 INSERT INTO Clientes (IDUsuario, IDCategoria, DNI, Nombre, Apellido, Telefono) VALUES
-(1, 1, 30111222, 'Juan', 'Perez', '1122334455'),
-(2, 2, 30999888, 'Ana', 'Garcia', '1144556677');
+(1, 1, 10200300, 'Cliente1', 'Uno', '1122223333'),
+(2, 2, 40500600, 'Cliente2', 'Dos', '1144445555'),
+(3, 3, 40500600, 'Cliente3', 'Tres', '1166667777')
 
 INSERT INTO Empleados (IDUsuario, Legajo, DNI, Nombre, Apellido) VALUES
-(3, 'L0001', 28123456, 'Carlos', 'Lopez')
+(4, 'L0001', 70800900, 'Empleado1', 'Uno'),
+(5, 'L0002', 10500900, 'Empleado2', 'Dos'),
+(6, 'L0003', 10500900, 'Empleado3', 'Tres'),
+(7, 'L0003', 10500900, 'Empleado3', 'Tres')
 
 INSERT INTO TiposIncidente (Nombre, Descripcion) VALUES
 ('Problema técnico', 'Falla en el servicio o software'),
-('Consulta general', 'Pregunta o duda sobre el servicio');
+('Consulta general', 'Pregunta o duda sobre el servicio')
 
 INSERT INTO PrioridadesIncidente (Nombre, Descripcion) VALUES
 ('Alta', 'Requiere atención inmediata'),
 ('Media', 'Puede esperar hasta 24h'),
-('Baja', 'Puede esperar hasta 72h');
+('Baja', 'Puede esperar hasta 72h')
 
 INSERT INTO Incidencias (IDEmpleado, IDCliente, IDTipo, IDPrioridad, EstadoActual, Descripcion, FechaYHoraCreacion) VALUES
-(1, 1, 1, 1, 'Reiniciando router...', 'No puedo acceder al sistema.', GETDATE());
+(1, 1, 1, 1, 'Reiniciando router...', 'No puedo acceder a internet.', GETDATE()),
+(2, 2, 1, 2, 'Instalando actualizacion...', 'La conexion es muy lenta.', GETDATE()),
+(3, 3, 2, 3, 'Consulta sobre facturacion', 'Esperando respuesta de gente de finanzas.', GETDATE())
 
 INSERT INTO Historiales (IDIncidencias, FechaCambio) VALUES
-(1, GETDATE());
+(1, GETDATE()),
+(2, GETDATE()),
+(3, GETDATE())
