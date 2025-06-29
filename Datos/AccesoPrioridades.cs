7d40f195a40a6dd1b44f506ebd5508512fb0eddc
facuello <facuello5@gmail.com>
2025-06-29 20:09:59 -0300

    upd Edicion de datos

--- Datos/AccesoPrioridades.cs
+++ Datos/AccesoPrioridades.cs
@@ -18,7 +18,32 @@ namespace Datos
             try
             {
                 datos.Conectar();
-                datos.Consultar("SELECT IDPrioridad, Nombre, Descripcion FROM PrioridadesIncidente");
+                datos.Consultar("SELECT IDPrioridad, Nombre, Descripcion FROM PrioridadesIncidente WHERE Eliminado = 0");
+                datos.Leer();
+                while (datos.Lector.Read())
+                {
+                    PrioridadesIncidente aux = new PrioridadesIncidente();
+                    aux.IDPrioridad = datos.Lector["IDPrioridad"] != DBNull.Value ? (int)datos.Lector["IDPrioridad"] : 0;
+                    aux.Nombre = datos.Lector["Nombre"] != DBNull.Value ? (string)datos.Lector["Nombre"] : string.Empty;
+                    aux.Descripcion = datos.Lector["Descripcion"] != DBNull.Value ? (string)datos.Lector["Descripcion"] : string.Empty;
+                    prioridades.Add(aux);
+                }
+            }
+            catch (Exception er)
+            {
+
+                throw er;
+            }
+            return prioridades;
+        }
+        public List<PrioridadesIncidente> ListarPrioridadesEliminadas()
+        {
+            datos = new AccesoDatos();
+            prioridades = new List<PrioridadesIncidente>();
+            try
+            {
+                datos.Conectar();
+                datos.Consultar("SELECT IDPrioridad, Nombre, Descripcion FROM PrioridadesIncidente WHERE Eliminado = 1");
                 datos.Leer();
                 while (datos.Lector.Read())
                 {
@@ -65,6 +90,65 @@ namespace Datos
                 datos.Consultar("INSERT INTO PrioridadesIncidente(Nombre, Descripcion) VALUES (@Nombre, @Descripcion)");
                 datos.setearParametro("@Nombre", nuevo.Nombre);
                 datos.setearParametro("@Descripcion", nuevo.Descripcion);
+                datos.EjecutarNonQuery();
+            }
+            catch (Exception er)
+            {
+                throw er;
+            }
+            finally
+            {
+                datos.Cerrar();
+            }
+        }
+        public void EliminarPrioridades(PrioridadesIncidente nuevo)
+        {
+            datos = new AccesoDatos();
+            try
+            {
+                datos.Conectar();
+                datos.Consultar("UPDATE PrioridadesIncidente SET Eliminado = 1 WHERE IDPrioridad = @IDPrioridad");
+                datos.setearParametro("@IDPrioridad", nuevo.IDPrioridad);
+                datos.EjecutarNonQuery();
+            }
+            catch (Exception er)
+            {
+                throw er;
+            }
+            finally
+            {
+                datos.Cerrar();
+            }
+        }
+        public void ReactivarPrioridades(PrioridadesIncidente nuevo)
+        {
+            datos = new AccesoDatos();
+            try
+            {
+                datos.Conectar();
+                datos.Consultar("UPDATE PrioridadesIncidente SET Eliminado = 0 WHERE IDPrioridad = @IDPrioridad");
+                datos.setearParametro("@IDPrioridad", nuevo.IDPrioridad);
+                datos.EjecutarNonQuery();
+            }
+            catch (Exception er)
+            {
+                throw er;
+            }
+            finally
+            {
+                datos.Cerrar();
+            }
+        }
+        public void ModificarPrioridades(PrioridadesIncidente nuevo)
+        {
+            datos = new AccesoDatos();
+            try
+            {
+                datos.Conectar();
+                datos.Consultar("UPDATE PrioridadesIncidente SET Nombre = @Nombre, Descripcion = @Descripcion WHERE IDPrioridad = @IDPrioridad");
+                datos.setearParametro("@Nombre", nuevo.Nombre);
+                datos.setearParametro("@Descripcion", nuevo.Descripcion);
+                datos.setearParametro("@IDPrioridad", nuevo.IDPrioridad);
                 datos.EjecutarNonQuery();
             }
             catch (Exception er)
---
+++ Dominio/EmailService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Dominio
+{
+    public class EmailService
+    {
+        private MailMessage email;
+        private SmtpClient server;
+
+        public EmailService()
+        {
+            server = new SmtpClient();
+            server.Credentials = new NetworkCredential("utncallcenter@gmail.com", "C3^U$54T@#E72*cY");
+            server.EnableSsl = true;
+            server.Port = 587;
+            server.Host = "smtp.gmail.com";
+        }
+
+        public void armarCorreo(string emailDestino, string asunto, string cuerpo)
+        {
+            email = new MailMessage();
+            email.From = new MailAddress("noresponder@callcenter.utn.com"); // Cambiar
+            email.To.Add(emailDestino);
+            email.Subject = asunto;
+            email.IsBodyHtml = true;
+            email.Body = "<h1>Resumen de Incidencia</h1> <br> "; // Cambiar: Elaborar HTML donde se provea el resumen de la incidencia.
+            //email.Body = cuerpo;
+
+        }
+
+        public void enviarEmail()
+        {
+            try
+            {
+                server.Send(email);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
